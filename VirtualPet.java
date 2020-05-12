612d8d19e6a0691b6c4cae9f8d809168824451ab
Dwright46 <61440666+Dwright46@users.noreply.github.com>
2020-05-11 21:36:47 -0400

    Added Project

---
+++ VirtualPet.java
@@ -0,0 +1,38 @@
+import java.time.Clock;
+import java.time.Instant;
+
+public class VirtualPet {
+    private int tick;
+    private int hunger;
+    private int thirst;
+    private int boredom;
+    private int tiredness;
+
+    private int xpos = 0, ypos =0;
+
+    public int getTick(){
+
+        // over the time the
+
+        // hunger = hunger + anotherVariable ... each one
+
+        System.out.println();
+        return tick;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
+
+    public VirtualPet(int tick, int boredom){
+        this.tick = tick;
+        this.boredom = boredom;
+
+        // method getPetStats
+                                           // sout"Press 1 to inc hunger "
+        // method feed  to dec. hunger ,, press (1)
+    }
+
+}
+
+
---
+++ VirtualPetApp.java
@@ -0,0 +1,9 @@
+
+public class VirtualPetApp {
+    public static void main(String[] args) {
+        VirtualPet myPet = new VirtualPet(25,10);
+        System.out.println(myPet.getTick());
+
+        System.out.println(myPet.getBoredom());
+    }
+}
